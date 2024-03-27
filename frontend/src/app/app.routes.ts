1b7370688c57f8e53eda1cb1b2280e2addb421af
Fernando Schimidt <40704350+FernandoSchimidt@users.noreply.github.com>
2024-03-27 10:54:50 -0300

    Envio do codigo via email para recuperar senha

--- frontend/src/app/app.routes.ts
+++
@@ -1,6 +0,0 @@
-import { Routes } from '@angular/router';
-import { EstadoListComponent } from './components/estado/estado-list/estado-list.component';
-
-export const routes: Routes = [
-  { path: 'estado', component: EstadoListComponent },
-];
